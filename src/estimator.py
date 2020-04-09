705ed82eb1051df6fff25b545b11d4c3efb27e74
odipojames <odipojames12@gmai.com>
2020-04-09 13:50:39 +0300

    [finishes 1-3 challenge]

--- src/estimator.py
+++ src/estimator.py
@@ -26,12 +26,8 @@ def estimator(data):
             impact["currentlyInfected"] * (2**(data["timeToElapse"] * 30) / 3))
     impact["severeCasesByRequestedTime"] = impact["infectionsByRequestedTime"] * 0.15
     severeImpact["severeCasesByRequestedTime"] = severeImpact["infectionsByRequestedTime"] * 0.15
-    impact["hospitalBedsByRequestedTime"] -
-        (data["totalHosiptalBeds"] * 0.35 -
-         impact["severeCasesByRequestedTime"])
-    severeImpact["hospitalBedsByRequestedTime"] -
-        (data["totalHosiptalBeds"] * 0.35 -
-         severeImpact["severeCasesByRequestedTime"])
+    impact["hospitalBedsByRequestedTime"]-(data["totalHosiptalBeds"]*0.35-impact["severeCasesByRequestedTime"])
+    severeImpact["hospitalBedsByRequestedTime"]-(data["totalHosiptalBeds"]*0.35-severeImpact["severeCasesByRequestedTime"])
     impact["casesForICUByRequestedTime"] = impact["infectionsByRequestedTime"] * 0.5
     severeImpact["casesForICUByRequestedTime"] = severeImpact["infectionsByRequestedTime"] * 0.5
 
