1b523a697f10aacd85d36e6fd8039c33b63918cc
odipojames <odipojames12@gmai.com>
2020-04-11 11:13:26 +0300

    challange 4 done

--- src/estimator.py
+++ src/estimator.py
@@ -26,21 +26,24 @@ def estimator(data):
 
     if data["periodType"].lower() == "months":
         impact["infectionsByRequestedTime"] = math.ceil(
-            impact["currentlyInfected"] * (2 ** (data["timeToElapse"] * 30) / 3)
+            impact["currentlyInfected"] *
+            (2 ** (data["timeToElapse"] * 30) / 3)
         )
         severeImpact["infectionsByRequestedTime"] = math.ceil(
-            impact["currentlyInfected"] * (2 ** (data["timeToElapse"] * 30) / 3)
+            impact["currentlyInfected"] *
+            (2 ** (data["timeToElapse"] * 30) / 3)
         )
+
     impact["severeCasesByRequestedTime"] = impact["infectionsByRequestedTime"] * 0.15
     severeImpact["severeCasesByRequestedTime"] = (
         severeImpact["infectionsByRequestedTime"] * 0.15
     )
-    impact["hospitalBedsByRequestedTime"] - (
+    impact["hospitalBedsByRequestedTime"] =
         data["totalHosiptalBeds"] * 0.35 - impact["severeCasesByRequestedTime"]
-    )
-    severeImpact["hospitalBedsByRequestedTime"] - (
-        data["totalHosiptalBeds"] * 0.35 - severeImpact["severeCasesByRequestedTime"]
-    )
+
+    severeImpact["hospitalBedsByRequestedTime"] = data["totalHosiptalBeds"] * 0.35 -
+    severeImpact["severeCasesByRequestedTime"]
+
     impact["casesForICUByRequestedTime"] = impact["infectionsByRequestedTime"] * 0.5
     severeImpact["casesForICUByRequestedTime"] = (
         severeImpact["infectionsByRequestedTime"] * 0.5
