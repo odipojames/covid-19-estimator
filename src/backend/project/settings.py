b18453cc2df043c8b28f0981f53889e6c128b2c3
odipojames <odipojames12@gmai.com>
2020-04-14 01:02:01 +0300

    fix

--- src/backend/project/settings.py
+++ src/backend/project/settings.py
@@ -72,6 +72,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
