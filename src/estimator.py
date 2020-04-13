a8c6cbd7097e0849a70a0933a90ff0c3ab93ceb8
odipojames <odipojames12@gmai.com>
2020-04-13 22:21:44 +0300

    fix

--- src/estimator.py
+++ src/estimator.py
@@ -16,72 +16,72 @@ def estimator(data):
 
     if data["periodType"].lower() == "days":
 
-        impact["infectionsByRequestedTime"] = float_rep(math.ceil(
+        impact["infectionsByRequestedTime"] = float_rep(int(
             float(impact["currentlyInfected"]) * 2 ** (data["timeToElapse"] / 3)))
-        severeImpact["infectionsByRequestedTime"] = float_rep(math.ceil(
+        severeImpact["infectionsByRequestedTime"] = float_rep(int(
             float(impact["currentlyInfected"]) * 2**(data["timeToElapse"] / 3)))
 
     if data["periodType"].lower() == "weeks":
-        impact["infectionsByRequestedTime"] = float_rep(math.ceil(
+        impact["infectionsByRequestedTime"] = float_rep(int(
             float(impact["currentlyInfected"]) * 2**(data["timeToElapse"] * 7 / 3)))
-        severeImpact["infectionsByRequestedTime"] = float_rep(math.ceil(
+        severeImpact["infectionsByRequestedTime"] = float_rep(int(
             float(impact["currentlyInfected"]) * 2**(data["timeToElapse"] * 7 / 3)))
 
     if data["periodType"].lower() == "months":
-        impact["infectionsByRequestedTime"] = float_rep(math.ceil(
+        impact["infectionsByRequestedTime"] = float_rep(int(
             float(impact["currentlyInfected"]) *
                   2**(data["timeToElapse"] * 30 / 3)))
 
-        severeImpact["infectionsByRequestedTime"] = float_rep(math.ceil(
+        severeImpact["infectionsByRequestedTime"] = float_rep(int(
             float(severeImpact["currentlyInfected"]) *
                   2**(data["timeToElapse"] * 30 / 3)))
 
     impact["severeCasesByRequestedTime"]=float_rep(
-        math.ceil(float(impact["infectionsByRequestedTime"]) * 0.15))
+        int(float(impact["infectionsByRequestedTime"]) * 0.15))
     severeImpact["severeCasesByRequestedTime"]=float_rep(
-        math.ceil(float(severeImpact["infectionsByRequestedTime"]) * 0.15))
+        int(float(severeImpact["infectionsByRequestedTime"]) * 0.15))
 
-    impact["hospitalBedsByRequestedTime"]=float_rep(math.ceil(float(
+    impact["hospitalBedsByRequestedTime"]=float_rep(int(float(
         data["totalHospitalBeds"]) * 0.35 - float(impact["severeCasesByRequestedTime"])))
 
-    severeImpact["hospitalBedsByRequestedTime"]=float_rep(math.ceil(float(
+    severeImpact["hospitalBedsByRequestedTime"]=float_rep(int(float(
         data["totalHospitalBeds"]) * 0.35 - float(severeImpact["severeCasesByRequestedTime"])))
 
     impact["casesForICUByRequestedTime"]=float_rep(
-        math.ceil(float(impact["infectionsByRequestedTime"]) * 0.5))
-    severeImpact["casesForICUByRequestedTime"]=float_rep(math.ceil(
+        int(float(impact["infectionsByRequestedTime"]) * 0.5))
+    severeImpact["casesForICUByRequestedTime"]=float_rep(int(
         float(severeImpact["infectionsByRequestedTime"]) * 0.5))
 
-    impact["casesForVentilatorsByRequestedTime"]=float_rep(math.ceil(
+    impact["casesForVentilatorsByRequestedTime"]=float_rep(int(
         float(impact["infectionsByRequestedTime"]) * 0.2))
-    severeImpact["casesForVentilatorsByRequestedTime"]=float_rep(math.ceil(
+    severeImpact["casesForVentilatorsByRequestedTime"]=float_rep(int(
         float(severeImpact["infectionsByRequestedTime"]) * 0.2))
 
     region_data = data.pop("region")
     if data["periodType"].lower() == "days":
-        impact["dollarsInFlight"]=float_rep(math.ceil(float(impact["infectionsByRequestedTime"]) *
+        impact["dollarsInFlight"]=float_rep(int(float(impact["infectionsByRequestedTime"]) *
                                             region_data["avgDailyIncomePopulation"] * region_data["avgDailyIncomeInUSD"] * data["timeToElapse"]))
 
-        severeImpact["dollarsInFlight"]=float_rep(math.ceil(float(severeImpact["infectionsByRequestedTime"]) * region_data["avgDailyIncomePopulation"] * region_data["avgDailyIncomeInUSD"]
+        severeImpact["dollarsInFlight"]=float_rep(int(float(severeImpact["infectionsByRequestedTime"]) * region_data["avgDailyIncomePopulation"] * region_data["avgDailyIncomeInUSD"]
             * data["timeToElapse"]))
     if data["periodType"].lower() == "weeks":
-        impact["dollarsInFlight"]=float_rep(math.ceil(float(impact["infectionsByRequestedTime"])
+        impact["dollarsInFlight"]=float_rep(int(float(impact["infectionsByRequestedTime"])
                 * region_data["avgDailyIncomePopulation"]
             * region_data["avgDailyIncomeInUSD"]
             * data["timeToElapse"]
             * 7))
-        severeImpact["dollarsInFlight"]=float_rep(math.ceil(float(severeImpact["infectionsByRequestedTime"]) * region_data["avgDailyIncomePopulation"]
+        severeImpact["dollarsInFlight"]=float_rep(int(float(severeImpact["infectionsByRequestedTime"]) * region_data["avgDailyIncomePopulation"]
             * region_data["avgDailyIncomeInUSD"]
             * data["timeToElapse"]
             * 7))
 
     if data["periodType"].lower() == "months":
-        impact["dollarsInFlight"]=float_rep(math.ceil(float(impact["infectionsByRequestedTime"])
+        impact["dollarsInFlight"]=float_rep(int(float(impact["infectionsByRequestedTime"])
                 * region_data["avgDailyIncomePopulation"]
             * region_data["avgDailyIncomeInUSD"]
             * data["timeToElapse"]
             * 30))
-        severeImpact["dollarsInFlight"]=float_rep(math.ceil(float(severeImpact["infectionsByRequestedTime"]) * region_data["avgDailyIncomePopulation"]
+        severeImpact["dollarsInFlight"]=float_rep(int(float(severeImpact["infectionsByRequestedTime"]) * region_data["avgDailyIncomePopulation"]
             * region_data["avgDailyIncomeInUSD"]
             * data["timeToElapse"]
             * 30))
